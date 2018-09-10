b583bd4275f6b3a8d46664a4575f1845a18aa6a8
bocaj5 <43143147+bocaj5@users.noreply.github.com>
2018-09-10 15:41:57 +0100

    V1.0.0

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = dicord.client()
+client = command.Bot(commands_prefix = "!")
+
+@client.event
+async def on_ready():
+    print("Bot Is Ready")
+
+@client.event
+async def on_message(message):
+    if message.content == "countdown":
+        await client.send_message(message.channel, ":Ready:")
+
+
+
+client.run("dpPzEujT_Xb0vQi9lF_6e2VgPhSBJ8cX")
